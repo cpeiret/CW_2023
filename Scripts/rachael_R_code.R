fb3395ccd3d930b4128a83959f4d61080d679bb2
RSanderson96 <r.sanderson4@newcastle.ac.uk>
2023-04-27 12:09:37 +0100

    code updates

--- Scripts/rachael_R_code.R
+++ Scripts/rachael_R_code.R
@@ -1,4 +1,4 @@
-Packages = c("plyr", "ggplot2","tmap", "basemaps", "raster", "geojsonsf", "biscale", "cowplot", "sf","dplyr")
+Packages = c("plyr", "ggplot2", "tmap", "basemaps", "raster", "geojsonsf", "corrplot", "biscale", "cowplot", "sf","dplyr", "tidyverse")
 
 ## Now load or install&load all
 package.check <- lapply(
@@ -14,7 +14,7 @@ package.check <- lapply(
 
 
 convert = read.csv("convert.csv")
-LSOA = st_read(dsn = (paste0(getwd(), "/LSOA")),layer="LSOA_2011_EW_BFC_V3")
+LSOA = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/LSOA.geojson")
 
 Index_Adult = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_adults_lsoa.geojson")%>%
   st_drop_geometry()
@@ -25,6 +25,9 @@ Index_Seniors = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/ma
 
 
 
+
+#Step 1 - which sub-populations are failed by the index?
+#Correlation matrix
 hh = read.csv("household_composition.csv")%>%
   separate(1, c("LSOA21CD", NA), sep = " : ")%>%
   select(1,
@@ -46,79 +49,98 @@ Sex = read.csv("sex.csv")%>%
          female_perc)
 ethnic = read.csv("ethic_group.csv")%>%
   select(LSOA21CD = 2,
-         white_perc)
+         white_perc)%>%
+  mutate(white_perc = 100 - white_perc)
 disability = read.csv("disability.csv")%>%
   separate(1, c("LSOA21CD", NA), sep = " : ")
 
+IMD_rank = read.csv("imd_score.csv")%>%
+  select(1,2,5)%>%
+  left_join(., read.csv("imd.csv"))%>%
+  rename(LSOA11CD = 1)%>%
+  left_join(., convert, by = "LSOA11CD")%>%
+  select(imd_score, imd_rank, LSOA21CD, LSOA11CD)
+
+SAMHI = read.csv("samhi.csv")%>%
+  filter(., year=="2019")%>%
+  rename(LSOA11CD = 1)%>%
+  mutate(samhi_rank = rank(-samhi_index))%>%
+  left_join(., convert, by = "LSOA11CD")%>%
+  select(samhi_index, samhi_rank, LSOA21CD, LSOA11CD)
+
+Liverpool= sf::st_read(dsn = (paste0(getwd(), "/LAD")),layer="LAD_DEC_2021_GB_BFC")%>%
+  filter(., LAD21NM == "Liverpool")
+
+
+
 Corr_data = hh%>%
-  left_join(., Sex)%>%
-  left_join(.,ethnic)%>%
-  left_join(., disability)%>%
+  left_join(., Sex, by = "LSOA21CD")%>%
+  left_join(.,ethnic, by = "LSOA21CD")%>%
+  left_join(., disability, by = "LSOA21CD")%>%
+  left_join(., IMD_rank, by = "LSOA21CD")%>%
+  left_join(., SAMHI, by = "LSOA21CD")%>%
   left_join(Index_Adult%>%
               select(LSOA21CD, adult_index = score_norm),. , by = "LSOA21CD")%>%
   left_join(Index_Child%>%
               select(LSOA21CD, child_index = score_norm),.,by = "LSOA21CD" )%>%
   left_join(Index_Seniors%>%
-              select(LSOA21CD, senior_index = score_norm),.,by = "LSOA21CD")
+              select(LSOA21CD, senior_index = score_norm),.,by = "LSOA21CD")%>%
+  #filter(!is.na(imd_score))%>%
+  #filter(!(LSOA21CD %in% c("E01034409", "E01034401", "E01034404")))
+  select(-LSOA11CD)%>%
+  group_by(LSOA21CD)%>%
+  summarise_all(median)
 
+Corr_data$imd_score[is.na(Corr_data$imd_score)]<- median(Corr_data$imd_score, na.rm = TRUE)
+Corr_data$samhi_index[is.na(Corr_data$samhi_index)]<-median(Corr_data$samhi_index, na.rm = TRUE)
+
+Corr_Vis = LSOA%>%
+  left_join(., Corr_data, by = "LSOA21CD")%>%
+  filter(!is.na(senior_index))
+
+
+tm_shape(Corr_Vis)+
+  tm_fill("child_index")
+
+rm(hh, Sex, ethnic, disability)
 write.csv(Corr_data, "git/corrplot_variables.csv")
 
 Corr_data%>%
-  select(-"LSOA21CD")%>%
+  select(-c("LSOA21CD", "imd_rank", "samhi_rank"))%>%
+  rename("Senior Index" = 1,
+         "Child Index" = 2,
+         "Adult Index" = 3,
+         "Households over 66 (%)" = 4,
+         "Households with children (%)" = 5,
+         "Female (%)" = 6, 
+         "Non-White (%)" = 7, 
+         "Registered Disabled (%)" = 8,
+         "IMD Score" = 9,
+         "Samhi Score" = 10)%>%
   cor(., method = "pearson")%>%
   corrplot.mixed(., tl.pos = 'lt', tl.col = "black", sig.level = 0.05) 
 
 
-
-
-
-
-#results_corrplot
-
-
-IMD_rank = read.csv("imd_score.csv")%>%
-  select(1,2,5)%>%
-  left_join(., read.csv("imd.csv"))%>%
-  rename(LSOA11CD = 1)
-SAMHI = read.csv("samhi.csv")%>%
-  filter(., year=="2019")%>%
-  rename(LSOA11CD = 1)%>%
-  mutate(samhi_rank = rank(-samhi_index))
-Liverpool= sf::st_read(dsn = (paste0(getwd(), "/LAD")),layer="LAD_DEC_2021_GB_BFC")%>%
-  filter(., LAD21NM == "Liverpool")
-
 #Bivariate plot
+bivariate = Corr_Vis%>%
+  select(c(2,13,14,15,16,17,18,19,20,21,22,23,24,25))%>%
+  bi_class(., x = imd_score, y = samhi_index, style = "quantile", dim = 3, keep_factors = FALSE, dig_lab = 3)
 
-bivariate = left_join(SAMHI%>%
-                        select(LSOA11CD, samhi_rank), 
-                      IMD_rank%>%
-                        select(LSOA11CD, imd_rank))%>%
-  left_join(LSOA,., by = "LSOA11CD")%>%
-  filter(., LSOA11CD %in% Index_Adult$LSOA11CD)
-
-
-data <- bivariate%>%
-  mutate(imd_rank = as.numeric(imd_rank),
-         samhi_rank = as.numeric(samhi_rank))%>%
-  bi_class(., x = imd_rank, y = samhi_rank, style = "quantile", dim = 3, keep_factors = FALSE, dig_lab = 3)
-
-#install.packages('ggsn')
-#library(ggsn)
 
 LSOA_Dec = bivariate%>%
-  mutate(imd_decile_liv = ntile(imd_rank, 10),
-         samhi_decile_liv = ntile(samhi_rank, 10))%>%
-  filter(., imd_decile_liv == 1 & samhi_decile_liv == 1)
+  mutate(imd_decile_liv = ntile(imd_score, 10),
+         samhi_decile_liv = ntile(samhi_index, 10))%>%
+  filter(., imd_decile_liv == 10 & samhi_decile_liv == 10)
 
 map <- ggplot() +
-  geom_sf(data = data, mapping = aes(fill = bi_class), color = NA, size = 0.05, show.legend = FALSE) +
+  geom_sf(data = bivariate, mapping = aes(fill = bi_class), color = "grey", size = 0.05, show.legend = FALSE) +
   bi_scale_fill(pal = "GrPink", dim = 3) +
   labs(
     title = "Comparing Mental Health and Deprivation"
   ) +
   bi_theme()+
   theme(plot.title = element_text(size = 14, face = "bold"))+
-  geom_sf(data = LSOA_Dec, fill = NA)
+  geom_sf(data = LSOA_Dec, fill = NA, color = "black")
 legend <- bi_legend(pal = "GrPink",
                     dim = 3,
                     xlab = "IMD Rank",
@@ -126,12 +148,11 @@ legend <- bi_legend(pal = "GrPink",
                     size = 8)
 
 
-Plot = ggdraw() +
-  draw_plot(map, 0, 0, 1, 1) +
-  draw_plot(legend, 0.65, 0.55, 0.17, 0.22)
-
+  Plot = ggdraw() +
+    draw_plot(map, 0, 0, 1, 1) +
+    draw_plot(legend, 0.65, 0.55, 0.17, 0.22)
 ggsave(
-  paste0("C:/Users/b9054751/OneDrive - Newcastle University/PhD/Data/CW_2023/bivariate.jpeg"),
+  paste0("C:/Users/b9054751/OneDrive - Newcastle University/PhD/Data/CW_2023/git/CW_2023/Figures/bivariate.jpeg"),
   plot = Plot
 )
 
@@ -164,70 +185,90 @@ Map1 =
 
 tmap_save(
   tm = Map1,
-  paste0("C:/Users/b9054751/OneDrive - Newcastle University/PhD/data/CW_2023/deciles_map.jpeg")
+  paste0("C:/Users/b9054751/OneDrive - Newcastle University/PhD/data/CW_2023/git/CW_2023/Figures/deciles_map.jpeg")
 )
 
+write.csv(LSOA_Dec%>%
+            st_drop_geometry(), "C:/Users/b9054751/OneDrive - Newcastle University/PhD/data/CW_2023/git/CW_2023/LSOA_Bottom_Deciles.csv")
 
 #Linear Regression
-
-
-Index_Child = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_children_lsoa.geojson")%>%
-  st_drop_geometry()
-Index_Seniors = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_seniors_lsoa.geojson")%>%
-  st_drop_geometry()
 
 Index_Adult = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_adults_lsoa.geojson")%>%
   st_drop_geometry()%>%
   left_join(.,convert, by = "LSOA21CD")%>%
-  filter(., !is.na(LSOA11CD))%>%
-  left_join(., SAMHI, by = "LSOA11CD")%>%
+  left_join(., SAMHI, by = "LSOA21CD")%>%
   left_join(., IMD_rank%>%
-              select(LSOA11CD, imd_rank), by = "LSOA11CD")%>%
-  select(c(1,2,3,4,5,6,7,9,8,25,26))
-  
+              select(LSOA21CD, imd_score), by = "LSOA21CD")%>%
+  select(c(1,2,3,4,5,6,7,9,8,10,21,18))%>%
+  group_by(LSOA21CD)%>%
+  summarise_all(median)
+
+Index_Adult$imd_score[is.na(Index_Adult$imd_score)]<- median(Index_Adult$imd_score, na.rm = TRUE)
+Index_Adult$samhi_index[is.na(Index_Adult$samhi_index)]<-median(Index_Adult$samhi_index, na.rm = TRUE)
 
 library(corrplot)
 cor(Index_Adult%>%
       select(-c(LSOA21CD, score_norm)), method = "pearson")%>%
   corrplot.mixed(., tl.pos = 'lt', tl.col = "black", sig.level = 0.05) 
 
-lmModel_adult <- lm(samhi_rank ~ . , data = Index_Adult%>%
-                    select(-c(LSOA21CD, score_norm, imd_rank)))
+lmModel_adult_samhi <- lm(samhi_index ~ . , data = Index_Adult%>%
+                    select(-c(LSOA21CD, score, score_norm, imd_score)))
+lmModel_adult_imd <- lm(imd_score ~ . , data = Index_Adult%>%
+                      select(-c(LSOA21CD, score, score_norm, samhi_index)))
 
 
 Index_Child = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_children_lsoa.geojson")%>%
   st_drop_geometry()%>%
   left_join(.,convert, by = "LSOA21CD")%>%
-  filter(., !is.na(LSOA11CD))%>%
-  left_join(., SAMHI, by = "LSOA11CD")%>%
+  left_join(., SAMHI, by = "LSOA21CD")%>%
   left_join(., IMD_rank%>%
-              select(LSOA11CD, imd_rank), by = "LSOA11CD")%>%
-  select(c(1,2,3,4,5,7,22, 23))
+              select(LSOA21CD, imd_score), by = "LSOA21CD")%>%
+  select(c(1,2,3,4,5,6,7,18,15))%>%
+  group_by(LSOA21CD)%>%
+  summarise_all(median)
+
+Index_Child$imd_score[is.na(Index_Child$imd_score)]<- median(Index_Child$imd_score, na.rm = TRUE)
+Index_Child$samhi_index[is.na(Index_Child$samhi_index)]<-median(Index_Child$samhi_index, na.rm = TRUE)
+
 
 library(corrplot)
 cor(Index_Child%>%
       select(-c(LSOA21CD, score_norm)), method = "pearson")%>%
   corrplot.mixed(., tl.pos = 'lt', tl.col = "black", sig.level = 0.05) 
 
-lmModel_child <- lm(samhi_rank ~ . , data = Index_Child%>%
-                      select(-c(LSOA21CD, score_norm, imd_rank)))
+
+lmModel_child_samhi <- lm(samhi_index ~ . , data = Index_Child%>%
+                            select(-c(LSOA21CD, score, score_norm, imd_score)))
+lmModel_child_imd <- lm(imd_score ~ . , data = Index_Child%>%
+                          select(-c(LSOA21CD, score, score_norm, samhi_index)))
+
+
+
 
 Index_Seniors = geojson_sf("https://raw.githubusercontent.com/cpeiret/CW_2023/main/Data/index_seniors_lsoa.geojson")%>%
   st_drop_geometry()%>%
   left_join(.,convert, by = "LSOA21CD")%>%
-  filter(., !is.na(LSOA11CD))%>%
-  left_join(., SAMHI, by = "LSOA11CD")%>%
+  left_join(., SAMHI, by = "LSOA21CD")%>%
   left_join(., IMD_rank%>%
-              select(LSOA11CD, imd_rank), by = "LSOA11CD")%>%
-  select(c(1,2,3,4,5,6,7,9,8,25, 26))
+              select(LSOA21CD, imd_score), by = "LSOA21CD")%>%
+  select(c(1,2,3,4,5,6,7,9,8,10,21,18))%>%
+  group_by(LSOA21CD)%>%
+  summarise_all(median)
+
+Index_Seniors$imd_score[is.na(Index_Seniors$imd_score)]<- median(Index_Seniors$imd_score, na.rm = TRUE)
+Index_Seniors$samhi_index[is.na(Index_Seniors$samhi_index)]<-median(Index_Seniors$samhi_index, na.rm = TRUE)
+
+
 
 library(corrplot)
 cor(Index_Seniors%>%
       select(-c(LSOA21CD, score_norm)), method = "pearson")%>%
   corrplot.mixed(., tl.pos = 'lt', tl.col = "black", sig.level = 0.05) 
 
-lmModel_seniors <- lm(samhi_rank ~ . , data = Index_Seniors%>%
-                      select(-c(LSOA21CD, score_norm, imd_rank)))
+lmModel_senior_samhi <- lm(samhi_index ~ . , data = Index_Seniors%>%
+                            select(-c(LSOA21CD, score, score_norm, imd_score)))
+lmModel_senior_imd <- lm(imd_score ~ . , data = Index_Seniors%>%
+                          select(-c(LSOA21CD, score, score_norm, samhi_index)))
 
 
 
@@ -243,9 +284,13 @@ library(ggplot2)
 library(ggstance)
 
 #Thesis Plot
-plot_summs(lmModel_adult , lmModel_child, lmModel_seniors,
+plot_summs(lmModel_adult_imd , lmModel_child_imd, lmModel_senior_imd,
                   model.names = c("Adults", "Children","Seniors"))
                   
+
+plot_summs(lmModel_adult_samhi , lmModel_child_samhi, lmModel_senior_samhi,
+           model.names = c("Adults", "Children","Seniors"))
+
 
 
 
@@ -253,62 +298,18 @@ plot_summs(lmModel_adult , lmModel_child, lmModel_seniors,
 library(texreg)
 # export to doc
 # export to html
-texreg::htmlreg(list(lmModel_adult , lmModel_child, lmModel_seniors),file='models_html.html',
+texreg::htmlreg(list(lmModel_adult_imd , lmModel_child_imd, lmModel_senior_imd),file='git/CW_2023/Figures/imd_models_html.html',
                 custom.model.names = c("Adults", "Children","Seniors"),
-                # custom.coef.names = c( "Intercept: Number of Tweets",
-                #                        "Total Population",
-                #                        "Area",
-                #                        "Age 16-29 (%)",
-                #                        "Age 30-44 (%)",
-                #                        "Age 45-59 (%)",
-                #                        "Age 60 (%)",
-                #                        "Workday Population",
-                #                        "Median House Price (£)",
-                #                        "Qualification Level 4 or above (%)",
-                #                        "Non-white Population (%)",
-                #                        "Female Population (%)",
-                #                        "Net Income (£)",
-                #                        "Household: Couple with dep. child"),
+                 custom.coef.names = c( "Intercept:IMD",
+                                        "Parks",
+                                        "Schools",
+                                        "Supermarkets",
+                                        "Pharmacies",
+                                        "ATMs",
+                                        "Cinemas",
+                                        "Gyms",
+                                        "Playgrounds",
+                                        "Pitches"),
                 leading.zero = T,
                 digits = 4)
 
-
-lmModel_scores = Index_Adult%>% select(LSOA21CD, samhi_rank, adult_index = score_norm)%>%
-  left_join(Index_Child%>%
-              select(LSOA21CD, child_index = score_norm))%>%
-  left_join(Index_Seniors%>%
-              select(LSOA21CD, senior_index = score_norm))
-
-
-  
-cor(lmModel_scores%>%
-      select(-c(LSOA21CD)), method = "pearson")%>%
-  corrplot.mixed(., tl.pos = 'lt', tl.col = "black", sig.level = 0.05) 
-
-
-lmModel_imd_seniors <- lm(samhi_rank ~ . , data = Index_Seniors%>%
-                        select(-c(LSOA21CD, score_norm)))
-lmModel_imd_adults <- lm(samhi_rank ~ . , data = Index_Adult%>%
-                            select(-c(LSOA21CD, score_norm)))
-lmModel_imd_child <- lm(samhi_rank ~ . , data = Index_Child%>%
-                           select(-c(LSOA21CD, score_norm)))
-
-
-texreg::htmlreg(list(lmModel_imd_adults , lmModel_imd_child, lmModel_imd_seniors),file='models_imd_html.html',
-                custom.model.names = c("Adults", "Children","Seniors"),
-                # custom.coef.names = c( "Intercept: Number of Tweets",
-                #                        "Total Population",
-                #                        "Area",
-                #                        "Age 16-29 (%)",
-                #                        "Age 30-44 (%)",
-                #                        "Age 45-59 (%)",
-                #                        "Age 60 (%)",
-                #                        "Workday Population",
-                #                        "Median House Price (£)",
-                #                        "Qualification Level 4 or above (%)",
-                #                        "Non-white Population (%)",
-                #                        "Female Population (%)",
-                #                        "Net Income (£)",
-                #                        "Household: Couple with dep. child"),
-                leading.zero = T,
-                digits = 4)
